967959dee35df5596336062952f6e9475a3ef29d
lou8 <lou8@illinois.edu>
2017-11-13 16:12:07 -0600

    reverse back pacage

--- starts-plugin/src/main/java/edu/illinois/starts/jdeps/BaseMojo.java
+++ starts-plugin/src/main/java/edu/illinois/starts/jdeps/BaseMojo.java
@@ -13,8 +13,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Scanner;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.logging.Level;
 
